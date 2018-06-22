3b56fa18adb6f9ac386460236745d30f2f73aff8
Glabrat <germanalbertolabra@gmail.com>
2018-06-22 11:43:04 -0400

    ejercicio 1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
 import 'whatwg-fetch';
+
+function createTable() {
+    let table;
+    let element;
+
+    table = `
+    <table class="home__table">
+    <thead>
+        <tr>
+        <th>Currency</th>
+        <th>15m</th>
+        <th>Last</th>
+        <th>Buy</th>
+        <th>Sell</th>
+        <th>Symbol</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+        <td></td>
+        </tr>
+    </tbody>
+    </table> 
+    `;
+
+    element = document.querySelector(".content");
+
+    element.innerHTML = table;
+}
+
+createTable();
