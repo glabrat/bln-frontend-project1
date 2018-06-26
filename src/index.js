5e05e011b59ce85884591dc5bfe577e53cd82d93
Glabrat <19995834+glabrat@users.noreply.github.com>
2018-06-26 11:17:36 -0400

    added files #39

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import 'whatwg-fetch';
+import { getCurrenciesValues, getBalance } from './dataRequest';
+
+function initializeBalance(balanceData) {
+    const balanceSpan = document.querySelector('.bitcoin--balance');
+    balanceSpan.textContent = balanceData.confirmed_balance;
+}
+
+function createTable() {
+
+}
